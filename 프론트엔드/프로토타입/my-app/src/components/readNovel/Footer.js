d1f9242e9755fe1799f54375f679b07e1306ef56
peterjr123 <jsp020206@gmail.com>
2021-07-18 01:38:12 +0900

    폰트와 css변경

--- 프론트엔드/프로토타입/my-app/src/components/readNovel/Footer.js
+++ 프론트엔드/프로토타입/my-app/src/components/readNovel/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
-    return <footer id="footer">Special Thanks: Yoon Sung Woo / Park Joon suk / Kim Ha Gook / Cho Ha Bin</footer>;
+    return <footer id="footer">Special Thanks: Yoon SungWoo / Park Joonsuk / Kim HaGook / Cho HaBin</footer>;
   }
 }
 
