fc3744fcfac7371c63b23e27941509690dc2fc26
Haguk Kim <hagukin@naver.com>
2021-07-25 22:56:13 +0900

    initial commit

---
+++ backend/post/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Post
+from .serializers import PostSerializer
+
+class ListPost(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class DetailPost(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
