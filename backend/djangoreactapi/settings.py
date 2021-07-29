b575888f3eaff5a3298d888dc8032c782ac88e63
hagukin <hagukin@naver.com>
2021-07-30 08:14:00 +0900

    change db to mysql, create new mysql server

--- backend/djangoreactapi/settings.py
+++ backend/djangoreactapi/settings.py
@@ -88,12 +88,14 @@ WSGI_APPLICATION = 'djangoreactapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+from . import sql_settings
+DATABASES = sql_settings.DATABASES
 
 
 # Password validation
