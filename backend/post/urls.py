e103e85ae9fda2ac9097a648bce36c9c4065095b
hagukin <hagukin@naver.com>
2021-07-30 10:07:37 +0900

    .

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.ListPost.as_view()),
     path('<int:pk>/', views.DetailPost.as_view()),
+    path('experimental/', views.PostAPIView.as_view()),
 ]
--- backend/post/views.py
+++ backend/post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from .models import Post
 from .serializers import PostSerializer
@@ -11,3 +12,18 @@ class ListPost(generics.ListCreateAPIView):
 class DetailPost(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+from rest_framework.views import APIView
+class PostAPIView(APIView):
+    def get(self, request):
+        post = Post.objects.all()
+        serializer = PostSerializer(post, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
