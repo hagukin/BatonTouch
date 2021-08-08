b29e501c6f1c4042eadbf2971d1e2ece6a38d0ae
chb09876 <chb09876@naver.com>
2021-08-08 18:34:26 +0900

    Link css update

--- 프론트엔드/프로토타입/my-app/src/new_components/Header/Nav.jsx
+++ 프론트엔드/프로토타입/my-app/src/new_components/Header/Nav.jsx
@@ -5,28 +5,39 @@ import "./Nav.css";
 class Nav extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      active: "main",
+    };
   }
   render() {
     return (
       <nav className="navWrapper">
         <ul className="navigation">
           <li>
-<<<<<<< HEAD
-            <Link to="/">메인</Link>
-=======
-            <Link to="/main" className="menus" className="active">메인</Link>
->>>>>>> d32483ad45b1cb01589efe4c1d7ac95814c101df
+            <Link to="/" className="menus" onClick={this.handleOnClick}>
+              메인
+            </Link>
           </li>
           <li>
-            <Link to="/read" className="menus">읽기</Link>
+            <Link to="/read" className="menus active" onClick={this.handleOnClick}>
+              읽기
+            </Link>
           </li>
           <li>
-            <Link to="/contribute" className="menus">참여</Link>
+            <Link to="/contribute" className="menus" onClick={this.handleOnClick}>
+              참여
+            </Link>
           </li>
         </ul>
       </nav>
     );
   }
+
+  handleOnClick = (e) => {
+    let dest = document.getElementsByClassName("active");
+    dest[0].className = dest[0].className.replace(" active", "");
+    e.target.className = e.target.className + " active";
+  };
 }
 
 export default Nav;
